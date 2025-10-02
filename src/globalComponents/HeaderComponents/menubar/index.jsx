f3b33a5fa5fdafa02ee5159903ffc2dd759b1b7b
Kodify <96272995+atikulla77@users.noreply.github.com>
2025-10-02 22:16:09 +0600

    reinstall node modules

--- src/globalComponents/HeaderComponents/menubar/index.jsx
+++ src/globalComponents/HeaderComponents/menubar/index.jsx
@@ -7,8 +7,8 @@ const Menubar = () => {
     <div className="bg-orange py-6">
       <Container>
         <div className="grid grid-cols-2 justify-between items-center text-white">
-          <MenuLeftBarLayout />
-          <MenuRightBarLayout />
+          <MenuLeftBarLayout/>
+          <MenuRightBarLayout/>
         </div>
       </Container>
     </div>
